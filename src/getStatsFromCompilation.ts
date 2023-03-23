9d32e1c1f930320da7f646017dd0a903c4cdc614
Scott Mikula <mikula@gmail.com>
2023-03-23 15:07:30 -0700

    Cleanup redundant data

--- src/getStatsFromCompilation.ts
+++ src/getStatsFromCompilation.ts
@@ -4,7 +4,6 @@ import { BundleStats, Chunk, ChunkGroup } from './types/BundleStats';
 export function getStatsFromCompilation(compilation: Compilation): any {
     return {
         chunkGroups: getChunkGroups(compilation),
-        namedChunkGroups: getNamedChunkGroups(compilation), // TOOD: Confirm that this is redundant
         chunks: getChunks(compilation),
         modules: getModules(compilation),
     };
@@ -12,15 +11,6 @@ export function getStatsFromCompilation(compilation: Compilation): any {
 
 function getChunkGroups(compilation: Compilation): ChunkGroup[] {
     return compilation.chunkGroups.map(cg => ({
-        id: cg.id,
-        name: cg.name || undefined,
-        children: [...cg.childrenIterable].map(cg2 => cg2.id),
-        chunks: cg.chunks.map(c => c.id!),
-    }));
-}
-
-function getNamedChunkGroups(compilation: Compilation): ChunkGroup[] {
-    return [...compilation.namedChunkGroups.values()].map(cg => ({
         id: cg.id,
         name: cg.name || undefined,
         children: [...cg.childrenIterable].map(cg2 => cg2.id),
@@ -47,10 +37,6 @@ function getModule(m: Module, compilation: Compilation): any {
     return {
         moduleType: m.constructor.name,
         readableIdentifier: m.readableIdentifier(compilation.requestShortener),
-
-        // TODO: Seems like this is not that interesting, since it can be derived from chunk data,
-        // and it doesn't include chuncks where this module is in a concatenated module
-        getModuleChunks: compilation.chunkGraph.getModuleChunks(m).map(c => c.id),
         modules: (m as any).modules?.map((m2: any) => getModule(m2, compilation)),
         size: m.size(),
     };
