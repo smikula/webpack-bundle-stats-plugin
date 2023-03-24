a7f1a37a6243270b16ccdd7d65e3fef65f51ce6e
Scott Mikula <mikula@gmail.com>
2023-03-23 17:18:24 -0700

    Add data for modules (#6)
    This adds a bunch of data about modules to the output `BundleStats`.

--- src/getStatsFromCompilation.ts
+++ src/getStatsFromCompilation.ts
@@ -1,10 +1,11 @@
-import { Compilation } from 'webpack';
-import { BundleStats, Chunk, ChunkGroup } from './types/BundleStats';
+import { Compilation, Module as RawModule } from 'webpack';
+import { BundleStats, Chunk, ChunkGroup, Module } from './types/BundleStats';
 
 export function getStatsFromCompilation(compilation: Compilation): BundleStats {
     return {
         chunkGroups: getChunkGroups(compilation),
         chunks: getChunks(compilation),
+        modules: getModules(compilation),
     };
 }
 
@@ -21,5 +22,22 @@ function getChunks(compilation: Compilation): Chunk[] {
     return [...compilation.chunks].map(c => ({
         id: c.id || '<null>',
         name: c.name || undefined,
+        files: [...c.files],
+        modules: compilation.chunkGraph
+            .getChunkModules(c)
+            .map(m => m.readableIdentifier(compilation.requestShortener)),
     }));
+}
+
+function getModules(compilation: Compilation): Module[] {
+    return [...compilation.modules].map(m => getModule(m, compilation));
+}
+
+function getModule(m: RawModule, compilation: Compilation): Module {
+    return {
+        moduleType: m.constructor.name,
+        readableIdentifier: m.readableIdentifier(compilation.requestShortener),
+        modules: (m as any).modules?.map((m2: RawModule) => getModule(m2, compilation)),
+        size: m.size(),
+    };
 }
